3a48a20ab56f85a2f97936888cf51fdd4563d181
adcribeiro <andreribeiro6@hotmail.com>
2023-06-22 19:08:14 -0300

    Final

--- TrilhaJavaBasico/ContaBanco/src/ContaTerminal.java
+++ TrilhaJavaBasico/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, informe o número da conta");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Por favor, informe o número da Agência");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, informe seu nome");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, informe o sado");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero
+                + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }

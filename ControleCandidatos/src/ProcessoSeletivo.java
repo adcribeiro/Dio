a2ffb4e9c34263845d356696e709c8cc6b23e765
adcribeiro <andreribeiro6@hotmail.com>
2024-09-16 23:04:11 -0300

    metodo entrandoEmContato

--- ControleCandidatos/src/ProcessoSeletivo.java
+++ ControleCandidatos/src/ProcessoSeletivo.java
@@ -1,15 +1,48 @@
+import javax.naming.SizeLimitExceededException;
+import javax.swing.text.DefaultEditorKit;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        /*
-         * analisarCandidato(1900.0);
-         * analisarCandidato(2200.0);
-         * analisarCandidato(2000.0);
-         */
+//         analisarCandidato(1900.0);
+//         analisarCandidato(2200.0);
+//         analisarCandidato(2000.0);
 
-        // selecaoCandidatos();
+
+        selecaoCandidatos();
+        System.out.println("");
         imprimirSelecionado();
+
+        System.out.println();
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGSUTO" };
+        for (String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas =1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+        }while (continuarTentando && tentativasRealizadas < 3 );
+
+        if (atendeu)
+            System.out.println("CONSEGIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas);
+    }
+
+    //metodo auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionado() {
